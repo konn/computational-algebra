13ed0f92bc6d231ad462ab0891bf19516a231f20
Hiromi Ishii <konn.jinro@gmail.com>
2013-10-26 13:54:52 +0900

    Removed Monomorphic.hs

--- Monomorphic.hs
+++
@@ -1,5 +0,0 @@
-{-# LANGUAGE DataKinds, ExistentialQuantification, FlexibleContexts, GADTs #-}
-{-# LANGUAGE PolyKinds, RankNTypes, TypeFamilies, TypeOperators            #-}
-{-# LANGUAGE UndecidableInstances, CPP                                          #-}
-module Monomorphic (module Data.Type.Monomorphic) where
-import Data.Type.Monomorphic
