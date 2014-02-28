7d43db5175f1da92d5210b23161f9c676b76e6c1
Hiromi Ishii <konn.jinro@gmail.com>
2014-02-28 21:46:31 +0900

    Changed the module name

--- Algebra/Field/Finite.hs
+++ Algebra/Field/Finite.hs
@@ -1,37 +1,49 @@
-{-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses #-}
-{-# LANGUAGE RankNTypes, ScopedTypeVariables, UndecidableInstances      #-}
-module Algebra.Field.Finite where
+{-# LANGUAGE DataKinds, FlexibleContexts, FlexibleInstances          #-}
+{-# LANGUAGE MultiParamTypeClasses, PolyKinds, RankNTypes            #-}
+{-# LANGUAGE ScopedTypeVariables, TypeFamilies, UndecidableInstances #-}
+module Algebra.Field.Finite (F(), withModulo) where
+import           Algebra.Ring.Noetherian
+import           Algebra.Wrapped
 import           Data.Proxy
 import           Data.Ratio
 import           Data.Reflection
-import qualified Numeric.Algebra as NA
+import qualified Data.Type.Natural       as TN
+import qualified Numeric.Algebra         as NA
 
 -- | @p@ should be prime, and not statically checked.
-newtype Finite p = Finite { runFinite :: Int }
+newtype F p = F { runF :: Int }
 
-instance Reifies p Int => Show (Finite p) where
-  showsPrec d n@(Finite p) = showsPrec d (p `mod` reflect n)
+instance Reifies p Int => Show (F p) where
+  showsPrec d n@(F p) = showsPrec d (p `mod` reflect n)
 
-withModulo :: forall p. Reifies p Int => Int -> Finite p
-withModulo i = Finite (i `mod` reflect (Proxy :: Proxy p))
+withModulo :: forall p. Reifies p Int => Int -> F p
+withModulo i = F (i `mod` reflect (Proxy :: Proxy p))
 
 asProxyOf :: f a -> Proxy a -> f a
 asProxyOf = const
 
-reifyMod :: Int -> (forall p. Reifies p Int => Finite p) -> Int
-reifyMod p a = reify p (runFinite . asProxyOf a)
+{-
+reifyMod :: Int -> (forall p. Reifies p Int => F p) -> Int
+reifyMod p a = reify p (runF . asProxyOf a)
+-}
+instance Reifies p Int => NoetherianRing (F p)
 
-instance Reifies p Int => Eq (Finite p) where
-  n == m = runFinite n `mod` reflect n == runFinite m `mod` reflect n
+instance Reifies p Int => Eq (F p) where
+  n == m = runF n `mod` reflect n == runF m `mod` reflect n
 
-instance Reifies p Int => Num (Finite p) where
-  fromInteger n = Finite $ fromInteger n `mod` reflect (Proxy :: Proxy p)
-  Finite a + Finite b = withModulo $ a + b
-  Finite a - Finite b = withModulo $ a - b
-  negate c = Finite $ reflect c - runFinite c
-  a * b = withModulo $ runFinite a * runFinite b
-  abs (Finite n) = withModulo $ abs n
-  signum (Finite n) = withModulo $ signum n
+instance Reifies p Int => Normed (F p) where
+  type Norm (F p) = Int
+  norm n@(F p) = p `mod` reflect n
+  liftNorm = F . (`mod` reflect (Proxy :: Proxy p))
+
+instance Reifies p Int => Num (F p) where
+  fromInteger n = F $ fromInteger n `mod` reflect (Proxy :: Proxy p)
+  F a + F b = withModulo $ a + b
+  F a - F b = withModulo $ a - b
+  negate c = F $ reflect c - runF c
+  a * b = withModulo $ runF a * runF b
+  abs (F n) = withModulo $ abs n
+  signum (F n) = withModulo $ signum n
 
 modPow :: (Integral a, Integral a1) => a -> a -> a1 -> a
 modPow i p = go i 1
@@ -39,61 +51,67 @@ modPow i p = go i 1
     go _ acc 0 = acc
     go b acc e = go ((b*b) `mod` p) (if e `mod` 2 == 1 then (acc * b) `mod` p else acc) (e `div` 2)
 
-pow :: (Integral a1, Reifies p Int) => Finite p -> a1 -> Finite p
-pow a n = withModulo $ modPow (runFinite a) (reflect a) n
+pow :: (Integral a1, Reifies p Int) => F p -> a1 -> F p
+pow a n = withModulo $ modPow (runF a) (reflect a) n
 
-instance Reifies p Int => NA.Additive (Finite p) where
+instance Reifies p Int => NA.Additive (F p) where
   (+) = (+)
 
-instance Reifies p Int => NA.Multiplicative (Finite p) where
+instance Reifies p Int => NA.Multiplicative (F p) where
   (*) = (*)
   pow1p n p = pow n (p + 1)
 
-instance Reifies p Int => NA.Monoidal (Finite p) where
+instance Reifies p Int => NA.Monoidal (F p) where
   zero = 0
 
-instance Reifies p Int => NA.LeftModule NA.Natural (Finite p) where
+instance Reifies p Int => NA.LeftModule NA.Natural (F p) where
   n .* p = fromIntegral n * p
 
-instance Reifies p Int => NA.RightModule NA.Natural (Finite p) where
+instance Reifies p Int => NA.RightModule NA.Natural (F p) where
   p *. n = fromIntegral n * p
 
-instance Reifies p Int => NA.LeftModule Integer (Finite p) where
+instance Reifies p Int => NA.LeftModule Integer (F p) where
   n .* p = fromIntegral n * p
 
-instance Reifies p Int => NA.RightModule Integer (Finite p) where
+instance Reifies p Int => NA.RightModule Integer (F p) where
   p *. n = fromIntegral n * p
 
-instance Reifies p Int => NA.Group (Finite p) where
+instance Reifies p Int => NA.Group (F p) where
   (-) = (-)
   negate = negate
 
-instance Reifies p Int => NA.Abelian (Finite p)
+instance Reifies p Int => NA.Abelian (F p)
 
-instance Reifies p Int => NA.Semiring (Finite p)
+instance Reifies p Int => NA.Semiring (F p)
 
-instance Reifies p Int => NA.Rig (Finite p) where
+instance Reifies p Int => NA.Rig (F p) where
   fromNatural = fromInteger . toInteger
 
-instance Reifies p Int => NA.Ring (Finite p) where
+instance Reifies p Int => NA.Ring (F p) where
   fromInteger = fromInteger
 
-instance Reifies p Int => NA.Unital (Finite p) where
+instance Reifies p Int => NA.Unital (F p) where
   one = 1
   pow = pow
 
-instance Reifies p Int => NA.Division (Finite p) where
+instance Reifies p Int => NA.Division (F p) where
   recip = recip
   (/)   = (/)
   (^)   = pow
 
-instance Reifies p Int => Fractional (Finite p) where
+instance Reifies p Int => Fractional (F p) where
   a / b = a * recip b
   fromRational r = withModulo (fromInteger $ numerator r) * recip (withModulo (fromInteger $ denominator r))
   recip 0 = error "divide by 0"
   recip n = pow n (reflect n - 2)
 
-instance Reifies p Int => NA.Commutative (Finite p)
+instance Reifies p Int => NA.Commutative (F p)
 
-instance Reifies p Int => NA.Characteristic (Finite p) where
+instance Reifies p Int => NA.Characteristic (F p) where
   char _ = fromIntegral $ reflect (Proxy :: Proxy p)
+
+instance Reifies TN.Z Int  where
+  reflect _ = 0
+
+instance Reifies n Int => Reifies (TN.S n) Int where
+  reflect _ = 1 + reflect (Proxy :: Proxy n)
