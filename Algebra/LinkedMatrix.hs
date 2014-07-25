5c35afff68df79bc44e0eac0f1553044d0ad97a8
Hiromi Ishii <konn.jinro@gmail.com>
2014-07-26 04:06:16 +0900

    * Implemented element, row, column wise cancelation * Changed to remove zero-element during scaling * TODO: make addDir and combineDir works correctly (zero / nonzero change)

--- Algebra/LinkedMatrix.hs
+++ Algebra/LinkedMatrix.hs
@@ -1,23 +1,27 @@
-{-# LANGUAGE BangPatterns, FlexibleContexts, MultiParamTypeClasses        #-}
-{-# LANGUAGE NamedFieldPuns, NoImplicitPrelude, NoMonomorphismRestriction #-}
-{-# LANGUAGE RankNTypes, ScopedTypeVariables, TemplateHaskell             #-}
+{-# LANGUAGE BangPatterns, FlexibleContexts, LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses, NamedFieldPuns, NoImplicitPrelude   #-}
+{-# LANGUAGE NoMonomorphismRestriction, RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell                                            #-}
 module Algebra.LinkedMatrix where
 import           Control.Applicative        ((<$>))
-import           Control.Lens
-import           Control.Monad              (zipWithM)
-import           Control.Monad              (forM)
+import           Control.Lens               hiding (index)
+import           Control.Monad              (forM, zipWithM)
 import           Control.Monad.Identity     (runIdentity)
 import           Control.Monad.State.Strict (runState)
 import           Data.IntMap                (empty)
-import           Data.IntMap.Strict         (IntMap)
-import           Data.IntMap.Strict         (insertWith)
-import           Data.IntMap.Strict         (alter)
+import           Data.IntMap.Strict         (IntMap, alter, insertWith)
+import           Data.IntMap.Strict         (mapMaybeWithKey)
 import qualified Data.IntMap.Strict         as IM
-import           Data.Vector                (Vector)
-import           Data.Vector                (create)
+import           Data.List                  (sort)
+import           Data.Maybe                 (fromJust)
+import           Data.Maybe                 (isJust)
+import           Data.Maybe                 (mapMaybe)
+import           Data.Vector                (Vector, create)
+import           Data.Vector                (modify)
+import           Data.Vector                (generate)
 import qualified Data.Vector                as V
 import qualified Data.Vector.Mutable        as MV
-import           Numeric.Algebra
+import           Numeric.Algebra            hiding ((<), (>))
 import           Numeric.Decidable.Zero     (isZero)
 import           Prelude                    hiding (Num (..))
 
@@ -95,28 +99,66 @@ swapper dir i j mat =
          | k == j = i
          | otherwise = k
 
-scaleRow :: Multiplicative a => a -> Int -> Matrix a -> Matrix a
-scaleRow a = mapMat (*a) Row
+scaleDir :: (DecidableZero a, Multiplicative a) => Direction -> a -> Int -> Matrix a -> Matrix a
+scaleDir dir a i mat
+  | otherwise = mapDir (*a) dir i mat
+  | isZero a  = clearDir dir i mat
 
-scaleCol :: Multiplicative a => a -> Int -> Matrix a -> Matrix a
-scaleCol a = mapMat (*a) Column
+clearAt :: Int -> Matrix a -> Matrix a
+clearAt k mat = mat & coefficients %~ go
+                    & forwardStart Column
+                    & forwardStart Row
+  where
+    !old = ((mat ^. coefficients) V.! k)
+           & colNext._Just %~ shifter
+           & rowNext._Just %~ shifter
+    forwardStart dir =
+      let l = (old ^. coordL dir)
+      in startL dir %~ mapMaybeWithKey
+                       (\d v -> if d == l && v == k
+                                then old ^. nextL dir
+                                else Just $ shifter v)
+    shiftDir sel = nextL sel %~ \case
+      Nothing -> Nothing
+      Just l ->
+        if l == k
+        then old ^. nextL sel
+        else Just $ shifter l
+    shifter n = if n < k then n else n - 1
+    go vs = generate (V.length vs - 1) $ \n ->
+      vs V.! (if n < k then n else n + 1) & shiftDir Column & shiftDir Row
 
-mapMat :: (a -> a) -> Direction
+clearDir :: Monoidal a => Direction -> Int -> Matrix a -> Matrix a
+clearDir dir i mat = foldl (flip clearAt) mat $ sort $ mapMaybe (fmap fst) $ V.toList $ igetDir dir i mat
+
+clearRow :: Monoidal a => Int -> Matrix a -> Matrix a
+clearRow = clearDir Row
+
+clearCol :: Monoidal a => Int -> Matrix a -> Matrix a
+clearCol = clearDir Column
+
+scaleRow :: (DecidableZero a, Multiplicative a) => a -> Int -> Matrix a -> Matrix a
+scaleRow = scaleDir Row
+
+scaleCol :: (DecidableZero a, Multiplicative a) => a -> Int -> Matrix a -> Matrix a
+scaleCol = scaleDir Column
+
+mapDir :: (a -> a) -> Direction
        -> Int -> Matrix a -> Matrix a
-mapMat f dir i mat = traverseMatrix mat tr dir i mat
+mapDir f dir i mat = traverseDir mat tr dir i mat
   where
     tr m k _ = m & coefficients . ix k . value %~ f
 
-traverseMatrix :: b -> (b -> Int -> Entry a -> b)
+traverseDir :: b -> (b -> Int -> Entry a -> b)
                -> Direction
                -> Int -> Matrix a -> b
-traverseMatrix ini f dir i mat =
-  runIdentity $  traverseMatrixM ini (\b j e -> return $ f b j e) dir i mat
+traverseDir ini f dir i mat =
+  runIdentity $  traverseDirM ini (\b j e -> return $ f b j e) dir i mat
 
-traverseMatrixM :: Monad m => b -> (b -> Int -> Entry a -> m b)
+traverseDirM :: Monad m => b -> (b -> Int -> Entry a -> m b)
                 -> Direction
                 -> Int -> Matrix a -> m b
-traverseMatrixM ini f dir i mat = go (IM.lookup i (mat^.startL dir)) ini
+traverseDirM ini f dir i mat = go (IM.lookup i (mat^.startL dir)) ini
   where
     vec = mat ^. coefficients
     go Nothing  b = return b
@@ -129,12 +171,24 @@ getDir :: Monoidal a
 getDir dir i mat =
   create $ do
     v <- MV.replicate (mat ^. lenL dir) zero
-    traverseMatrixM v trav dir i mat
+    traverseDirM () (trav v) dir i mat
+    return v
   where
-    trav v _ ent = MV.write v (ent ^. nthL dir) (ent ^. value) >> return v
+    trav v _ _ ent = MV.write v (ent ^. nthL dir) (ent ^. value)
+
+igetDir :: Monoidal a
+       => Direction -> Int -> Matrix a -> Vector (Maybe (Int, Entry a))
+igetDir dir i mat =
+  create $ do
+    v <- MV.replicate (mat ^. lenL dir) Nothing
+    traverseDirM () (trav v) dir i mat
+    return v
+  where
+    trav v _ k ent = MV.write v (ent ^. nthL dir) (Just (k, ent))
 
 getRow :: Monoidal a => Int -> Matrix a -> Vector a
 getRow = getDir Row
+
 getCol :: Monoidal a => Int -> Matrix a -> Vector a
 getCol = getDir Column
 
@@ -160,3 +214,42 @@ startL Column = colStart
 nextL :: Direction -> Lens' (Entry a) (Maybe Int)
 nextL Row    = rowNext
 nextL Column = colNext
+
+addDir :: Additive a => Direction -> Vector a -> Int -> Matrix a -> Matrix a
+addDir dir vec i mat = mat & coefficients %~ go
+  where
+    go = modify $ \mv -> do
+      traverseDirM () (\_ k e -> MV.write mv k (e & value %~ (+ vec V.! (e ^. nthL dir)))) dir i mat
+
+addRow :: Additive a => Vector a -> Int -> Matrix a -> Matrix a
+addRow = addDir Row
+
+addCol :: Additive a => Vector a -> Int -> Matrix a -> Matrix a
+addCol = addDir Column
+
+inBound :: (Int, Int) -> Matrix a -> Bool
+inBound (i, j) mat = 0 <= i && i < mat ^. height && 0 <= j && j < mat ^. width
+
+index :: Monoidal a => IM.Key -> Int -> Matrix a -> Maybe a
+index i j mat
+  | not $ inBound (i, j) mat = Nothing
+  | otherwise = Just $ go (IM.lookup i $ mat ^. rowStart)
+  where
+    go Nothing  = zero
+    go (Just k) =
+      let e = (mat ^. coefficients) V.! k
+      in if e^.idx._2 == j
+         then e ^. value
+         else go (e^.rowNext)
+
+(!) :: Monoidal a => Matrix a -> (Int, Int) -> a
+(!) a (i, j) = fromJust $ index i j a
+
+combineDir :: (Multiplicative a, Monoidal a) => Direction -> a -> Int -> Int -> Matrix a -> Matrix a
+combineDir dir alpha i j mat = mat & coefficients %~ go
+  where
+    go = modify $ \mv -> do
+      let !is = getDir dir i mat
+      traverseDirM () (\_ k e -> MV.write mv k (e & value %~ (+ (alpha * is V.! (e ^. nthL dir))))) dir j mat
+
+
