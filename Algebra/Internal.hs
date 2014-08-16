f11245cfa29cf39805052dd9999d7fb893f80108
Hiromi Ishii <konn.jinro@gmail.com>
2014-08-16 17:28:02 +0900

    modified signature for withSingI for GHC 7.6

--- Algebra/Internal.hs
+++ Algebra/Internal.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE CPP, DataKinds, FlexibleContexts, FlexibleInstances, GADTs #-}
-{-# LANGUAGE MultiParamTypeClasses, PolyKinds, RankNTypes               #-}
-{-# LANGUAGE StandaloneDeriving, TypeFamilies, TypeOperators            #-}
+{-# LANGUAGE CPP, DataKinds, FlexibleContexts, FlexibleInstances, GADTs   #-}
+{-# LANGUAGE KindSignatures, MultiParamTypeClasses, PolyKinds, RankNTypes #-}
+{-# LANGUAGE StandaloneDeriving, TypeFamilies, TypeOperators              #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 module Algebra.Internal ( toProxy, module Data.Proxy, WrappedField(..)
                         , withSingI) where
@@ -8,12 +8,14 @@ import Algebra.Instances ()
 import Algebra.Wrapped
 import Data.Proxy
 import Data.Singletons
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ < 708
+import Data.Type.Natural (SNat)
+
+withSingI :: SNat a -> t -> t
+withSingI n f = case singInstance n of
+  SingInstance -> f
+#endif
 
 toProxy :: a -> Proxy a
 toProxy _ = Proxy
 
-#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ < 708
-withSingI :: Sing n -> (SingI n => r) -> r
-withSingI n f = case singInstance n of
-  SingInstance -> f
-#endif
