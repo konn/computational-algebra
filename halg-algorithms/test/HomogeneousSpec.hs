a82af1136837d652d5def22ac6951cd96824715f
Hiromi ISHII <konn.jinro@gmail.com>
2018-03-13 21:26:20 +0900

    Testing with Singular

--- halg-algorithms/test/HomogeneousSpec.hs
+++ halg-algorithms/test/HomogeneousSpec.hs
@@ -1,37 +1,41 @@
-{-# LANGUAGE DataKinds, ExplicitNamespaces, GADTs, PatternSynonyms #-}
+{-# LANGUAGE DataKinds, ExplicitNamespaces, GADTs, OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms                                         #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 module HomogeneousSpec where
-import Algebra.Algorithms.Groebner
-import Algebra.Algorithms.Groebner.Homogeneous
-import Algebra.Internal                        (pattern (:<), KnownNat,
-                                                pattern NilL, SNat, sSucc)
-import Algebra.Ring.Ideal
-import Algebra.Ring.Polynomial
-import Utils
-
+import           Algebra.Algorithms.Groebner
+import           Algebra.Algorithms.Groebner.Homogeneous
+import           Algebra.Bridge.Singular
+import           Algebra.Internal                        (pattern (:<),
+                                                          KnownNat,
+                                                          pattern NilL, SNat,
+                                                          sSucc)
+import           Algebra.Ring.Ideal
+import           Algebra.Ring.Polynomial
 import           Control.Monad
-import qualified Data.Foldable          as F
-import           Data.List              (delete)
-import qualified Data.Sized.Builtin     as SV
-import           Numeric.Field.Fraction (Fraction)
+import qualified Data.Foldable                           as F
+import           Data.List                               (delete)
+import qualified Data.Sized.Builtin                      as SV
+import           Numeric.Field.Fraction                  (Fraction)
 import           Test.Hspec
 import           Test.Hspec.QuickCheck
+import           Test.HUnit.Lang
 import           Test.QuickCheck
 import           Utils
+
 
 asGenListOf :: Gen [a] -> a -> Gen [a]
 asGenListOf = const
 
 spec :: Spec
-spec = do
+spec =
   describe "calcHomogeneousGroebnerBasis" $ modifyMaxSize (const 4) $ modifyMaxSuccess (const 25) $ do
     prop "passes S-test" $
       checkForArity [2..3] prop_passesSTest
-    prop "divides all original generators" $
+    prop "includes the original ideal" $
       checkForArity [2..3] prop_groebnerDivsOrig
-    it "generates the same ideal as original" $ do
-      pendingWith "need example"
+    prop "is included in the orignal ideal" $
+      checkForArity [2..3] prop_groebnerIncluded
 
 prop_passesSTest :: KnownNat n => SNat n -> Property
 prop_passesSTest sdim =
@@ -46,3 +50,15 @@ prop_groebnerDivsOrig sdim =
   let ideal = map homogenize jdeal
       gs = unsafeCalcHomogeneousGroebnerBasis $ toIdeal ideal
   in all ((== 0) . (`modPolynomial` gs)) ideal
+
+prop_groebnerIncluded :: KnownNat n => SNat n -> Property
+prop_groebnerIncluded sdim =
+  forAll (elements [3..15]) $ \count ->
+  forAll (vectorOf count (polynomialOfArity sdim)) $ \jdeal -> do
+  let ideal = map homogenize jdeal
+      gs = unsafeCalcHomogeneousGroebnerBasis $ toIdeal ideal
+  is <- evalSingularIdealWith [] [] $
+        funE "reduce" [idealE' $ toIdeal gs, funE "groebner" [idealE' $ toIdeal ideal]]
+  if all (==0) is
+    then return ()
+    else assertFailure "Non-zero element found"
