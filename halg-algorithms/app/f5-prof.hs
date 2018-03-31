eb7ef61fad4dc5eabf46adb1924a7880c8d90c58
Hiromi ISHII <konn.jinro@gmail.com>
2018-04-01 02:43:26 +0900

    Changes the test-set for F5 profiling

--- halg-algorithms/app/f5-prof.hs
+++ halg-algorithms/app/f5-prof.hs
@@ -10,5 +10,10 @@ i2 =  [35 * y^4 - 30*x*y^2 - 210*y^2*z + 3*x^2 + 30*x*z - 105*z^2 +140*y*t - 21*
       ]
      where [t,u,x,y,z] = vars
 
+i3 :: [OrderedPolynomial (Fraction Integer) Grevlex 4]
+i3 = [ x^31 - x^6 - x- y, x^8 - z, x^10 -t]
+  where
+    [t,x,y,z] = vars
+
 main :: IO ()
-main = f5 (toIdeal i2) `deepseq` return ()
+main = f5 (toIdeal i3) `deepseq` return ()
