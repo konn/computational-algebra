cef0fbda30389832dad0101b0e0fb1c961932db9
Hiromi ISHII <konn.jinro@gmail.com>
2020-07-09 00:38:19 +0900

    More robust inspection test

--- halg-core-test/test/Algebra/Field/PrimeSpec.hs
+++ halg-core-test/test/Algebra/Field/PrimeSpec.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE DataKinds, DerivingStrategies, GADTs                          #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving, KindSignatures, NoImplicitPrelude #-}
-{-# LANGUAGE PolyKinds, RankNTypes, ScopedTypeVariables, TemplateHaskell   #-}
-{-# LANGUAGE TypeApplications                                              #-}
-{-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, KindSignatures, MagicHash         #-}
+{-# LANGUAGE NoImplicitPrelude, PolyKinds, RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell, TypeApplications                             #-}
+{-# OPTIONS_GHC -Wno-orphans -O2 #-}
 {-# OPTIONS_GHC -dsuppress-idinfo -dsuppress-coercions
       -dsuppress-type-applications
       -dsuppress-module-prefixes -dsuppress-type-signatures
@@ -16,7 +16,10 @@ import           AlgebraicPrelude
 import qualified AlgebraicPrelude          as NA
 import           Data.Proxy                (Proxy (..))
 import           Data.Singletons.Prelude
-import           GHC.TypeLits              (Nat)
+import           GHC.Base                  (modInt#)
+import           GHC.Integer
+import           GHC.TypeLits              (Nat, natVal)
+import           Language.Haskell.TH
 import           Math.NumberTheory.Primes
 import qualified Prelude                   as P
 import           Test.Hspec
@@ -31,6 +34,10 @@ bigEnoughPrime :: Prime Integer
 bigEnoughPrime = precPrime $ floor @Double
   $ sqrt $ fromIntegral $ maxBound @Int
 
+type LargeP = $(litT $ numTyLit $ unPrime
+  $ nextPrime $ floor @Double
+  $ sqrt $ fromIntegral $ maxBound @Int)
+
 n102F59 :: F 59
 n102F59 = 102
 
@@ -43,9 +50,19 @@ f59AddPrelude = (P.+)
 f59AddAlgebra :: F 59 -> F 59 -> F 59
 f59AddAlgebra = (NA.+)
 
-f59AddManual :: F 59 -> F 59 -> F 59
-f59AddManual = unsafeCoerce $ \(l :: Int) (r :: Int) ->
+f59AddManual :: Int -> Int -> Int
+f59AddManual = \l r ->
   (l + r) `mod` 59
+
+fLargeAddPrelude :: F LargeP -> F LargeP -> F LargeP
+fLargeAddPrelude = (P.+)
+
+fLargeAddAlgebra :: F LargeP -> F LargeP -> F LargeP
+fLargeAddAlgebra = (NA.+)
+
+fLargeAddManual :: Integer -> Integer -> Integer
+fLargeAddManual = \ l r ->
+  (l + r) `mod` natVal @LargeP Proxy
 
 checkInspection
   :: IT.Result -> Expectation
@@ -53,42 +70,100 @@ checkInspection IT.Success{} = pure ()
 checkInspection (IT.Failure msg) =
   fail msg
 
+n43 :: Int
+n43 = 43
+
+litLarge :: F LargeP
+litLarge = $(
+  let p = unPrime $ nextPrime $ floor @Double
+        $ sqrt $ fromIntegral $ maxBound @Int
+  in litE $ integerL $ p*3 `div` 2
+  )
+
+litLargeAnswer :: Integer
+litLargeAnswer = $(
+  let p = unPrime $ nextPrime $ floor @Double
+        $ sqrt $ fromIntegral $ maxBound @Int
+  in litE $ integerL $ (p*3 `div` 2) `mod` p
+  )
+
 spec :: Spec
 spec = do
   describe "F p" $ do
     prop "is a field (for small primes)" $ \(SmallPrime p) ->
           tabulate "p" [show p] $
           reifyPrimeField p $ property . isField
+    prop "is a field (for small, but medium primes)" $ \(MediumPrime p) ->
+          tabulate "p" [show p] $
+          reifyPrimeField p $ property . isField
     prop "is a field (for big primes)" $ \(BigPrime p) ->
       tabulate "p" [show p] $
       reifyPrimeField p $ property . isField
     describe "optimisation for small primes (F 59)" $ do
-      it "literal doesn't contain type-natural comparison"
-        $ checkInspection $(inspectTest $ 'n102F59 `doesNotUse` 'SLT)
+      describe "literal" $ do
+        it "doesn't contain type-classes"
+          $ checkInspection $(inspectTest $ hasNoTypeClasses 'n102F59)
+        it "is an immediate value modulo casting"
+          $ checkInspection $(inspectTest $ 'n102F59 ==- 'n43)
       describe "(Prelude.+)" $ do
-        it "doesn't contain type-natural comparison"
-          $ checkInspection $(inspectTest $ 'f59AddPrelude `doesNotUse` 'SLT)
-        it "has the same core as \a b -> (a + b) `mod` 59"
-          $ checkInspection $(inspectTest $ 'f59AddPrelude IT.=== 'f59AddManual)
+        it "has the same core representation as (NA.+) modulo casting" $
+          checkInspection $(inspectTest $ 'f59AddPrelude ==- 'f59AddAlgebra)
       describe "(NA.+)" $ do
+        it "doesn't contain type-classes" $ do
+          checkInspection $(inspectTest $ hasNoTypeClasses 'f59AddAlgebra)
         it "doesn't contain type-natural comparison"
           $ checkInspection $(inspectTest $ 'f59AddAlgebra `doesNotUse` 'SLT)
+        it "doesn't contain Integer type" $
+          checkInspection $(inspectTest $ 'f59AddAlgebra `hasNoType` ''Integer)
+        it "doesn't contain modInteger operation"
+          $ checkInspection $(inspectTest $ 'f59AddAlgebra `doesNotUse` 'modInteger)
         it "has the same core as \a b -> (a + b) `mod` 59"
-          $ checkInspection $(inspectTest $ 'f59AddAlgebra IT.=== 'f59AddManual)
+          $ checkInspection $(inspectTest $ 'f59AddAlgebra ==- 'f59AddManual)
+    describe ("optimisation for big prime (F " ++ show (natVal @LargeP Proxy) ++ ")") $ do
+      describe "literal" $ do
+        it "doesn't contain type-classes"
+          $ checkInspection $(inspectTest $ hasNoTypeClasses 'litLarge)
+        it "is an immediate value modulo casting"
+          $ checkInspection $(inspectTest $ 'litLarge ==- 'litLargeAnswer)
+      describe "(Prelude.+)" $ do
+        it "has the same core representation as (NA.+) modulo casting" $
+          checkInspection $(inspectTest $ 'fLargeAddPrelude ==- 'fLargeAddAlgebra)
+      describe "(NA.+)" $ do
+        it "doesn't contain type-classes" $ do
+          checkInspection $(inspectTest $ hasNoTypeClasses 'fLargeAddAlgebra)
+        it "doesn't contain type-natural comparison"
+          $ checkInspection $(inspectTest $ 'fLargeAddAlgebra `doesNotUse` 'SLT)
+        it "doesn't contain Integer type" $
+          checkInspection $(inspectTest $ 'fLargeAddAlgebra `hasNoType` ''Int)
+        it "doesn't contain modInt# operation"
+          $ checkInspection $(inspectTest $ 'fLargeAddAlgebra `doesNotUse` 'modInt#)
+        it "has the same core as \a b -> (a + b) `mod` p"
+          $ checkInspection $(inspectTest $ 'fLargeAddAlgebra ==- 'fLargeAddManual)
 
 newtype SmallPrime = SmallPrime { runSmallPrime :: Integer }
   deriving newtype (Show)
 
 instance Arbitrary SmallPrime where
   arbitrary = SmallPrime . unPrime
-    <$> QC.elements (take 1000 primes)
+    <$> QC.elements (take 10 primes)
+
+newtype MediumPrime = MediumPrime { runMediumPrime :: Integer }
+  deriving newtype (Show)
+
+instance Arbitrary MediumPrime where
+  arbitrary = MediumPrime . unPrime
+    <$> QC.elements
+        (take 10 [nextPrime
+          $ floor @Double $ sqrt
+          $ fromIntegral $ unPrime bigEnoughPrime
+          .. ])
 
 newtype BigPrime = BigPrime { runBigPrime :: Integer }
   deriving newtype (Show)
 
 instance Arbitrary BigPrime where
   arbitrary = BigPrime . unPrime
-    <$> QC.elements (take 1000 [bigEnoughPrime ..])
+    <$> QC.elements (take 10 [bigEnoughPrime ..])
 
 isField :: IsPrimeChar (p :: Nat) => Proxy (F p) -> F p -> F p -> F p -> Property
 isField _ e f g =
