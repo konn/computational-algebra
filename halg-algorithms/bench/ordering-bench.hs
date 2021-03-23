76f1c63afc1e2ad69afcc116341ee61cebf57a31
Hiromi ISHII <konn.jinro@gmail.com>
2021-03-23 19:43:55 +0900

    Disables ordering-bench

--- halg-algorithms/bench/ordering-bench.hs
+++ halg-algorithms/bench/ordering-bench.hs
@@ -1,21 +1,33 @@
-{-# LANGUAGE BangPatterns, DataKinds, FlexibleContexts, FlexibleInstances #-}
-{-# LANGUAGE GADTs, MultiParamTypeClasses, NoImplicitPrelude              #-}
-{-# LANGUAGE NoMonomorphismRestriction, OverloadedStrings, PolyKinds      #-}
-{-# LANGUAGE TypeFamilies, UndecidableInstances, ViewPatterns             #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 {-# OPTIONS_GHC -fno-warn-type-defaults -fno-warn-orphans -Wall #-}
+
 module Main where
-import           Algebra.Algorithms.Groebner
-import           Algebra.Algorithms.Groebner.Signature
-import           Algebra.Internal
-import           Algebra.Prelude.Core
-import           Control.Foldl                         (Fold)
-import qualified Control.Foldl                         as Fl
-import           Control.Lens                          (_1, _2)
-import           Data.Monoid                           (Dual (..))
-import           Data.Sequence                         (Seq ((:<|), (:|>)))
-import qualified Data.Sequence                         as Seq
-import           Data.Sized                    (unsized)
-import           Gauge.Main
+
+import Algebra.Algorithms.Groebner
+import Algebra.Algorithms.Groebner.Signature
+import Algebra.Internal
+import Algebra.Prelude.Core
+import Control.Foldl (Fold)
+import qualified Control.Foldl as Fl
+import Control.Lens (_1, _2)
+import Data.Monoid (Dual (..))
+import Data.Sequence (Seq ((:<|), (:|>)))
+import qualified Data.Sequence as Seq
+import Data.Sized (unsized)
+import qualified Data.Vector.Unboxed as U
+import Gauge.Main
 
 type Comparer = Fold (Int, Int) Ordering
 
@@ -34,20 +46,20 @@ data FoldlGrevlex = FoldlGrevlex
 
 instance SingI n => IsOrder n FoldlGrevlex where
   cmpMonomial _ (unsized -> m) (unsized -> n) =
-    Fl.fold  (gradeF <> revlexF) $ Seq.zip m n
+    Fl.fold (gradeF <> revlexF) $ Seq.zip m n
   {-# INLINE cmpMonomial #-}
 
 instance SingI n => IsMonomialOrder n FoldlGrevlex
 
-newtype Seq' a = Seq' { runSeq' :: Seq a }
+newtype Seq' a = Seq' {runSeq' :: Seq a}
 
 instance Foldable Seq' where
   foldl f z (Seq' (as :|> a)) = f (foldl f z (Seq' as)) a
-  foldl _ z _          = z
+  foldl _ z _ = z
   foldr f z (Seq' (a :<| as)) = f a (foldr f z (Seq' as))
-  foldr _ z _                 = z
+  foldr _ z _ = z
   foldMap f (Seq' (as :|> a)) = foldMap f (Seq' as) <> f a
-  foldMap _ _                 = mempty
+  foldMap _ _ = mempty
 
 data Seqs a where
   Seqs :: Seq a -> Seq b -> Seqs (a, b)
@@ -62,15 +74,14 @@ foldlSeq :: Fold a b -> Seq a -> b
 foldlSeq (Fl.Fold step begin done) s = done $! loop s
   where
     loop (as :|> a) = (step $ loop as) $! a
-    loop _          = begin
-
+    loop _ = begin
 {-# INLINE foldlSeq #-}
 
 data FoldlSeq'Grevlex = FoldlSeq'Grevlex
 
 instance SingI n => IsOrder n FoldlSeq'Grevlex where
   cmpMonomial _ (unsized -> m) (unsized -> n) =
-    foldlSeq  (gradeF <> revlexF) $ Seq.zip m n
+    foldlSeq (gradeF <> revlexF) $ U.zip m n
   {-# INLINE cmpMonomial #-}
 
 instance SingI n => IsMonomialOrder n FoldlSeq'Grevlex
@@ -82,47 +93,52 @@ foldlSeqs (Fl.Fold step begin done) (Seqs as bs) =
   done $! loop as bs
   where
     loop (xs :|> x) (ys :|> y) = step (loop xs ys) (x, y)
-    loop _          _          = begin
+    loop _ _ = begin
 {-# INLINE foldlSeqs #-}
 
 instance SingI n => IsOrder n FoldlSeqsGrevlex where
   cmpMonomial _ (unsized -> m) (unsized -> n) =
-    foldlSeqs  (gradeF <> revlexF) $ Seqs m n
+    foldlSeqs (gradeF <> revlexF) $ Seqs (toSeq m) (toSeq n)
   {-# INLINE cmpMonomial #-}
 
 instance SingI n => IsMonomialOrder n FoldlSeqsGrevlex
 
 grevlexHW :: Seq Int -> Seq Int -> Int -> Int -> Ordering -> Ordering
-grevlexHW (as :|> a) (bs :|> b)  !accl !accr EQ =
+grevlexHW (as :|> a) (bs :|> b) !accl !accr EQ =
   grevlexHW as bs (accl + a) (accr + b) $ compare b a
 grevlexHW as bs !accl !accr cmp = compare (sum as + accl) (sum bs + accr) <> cmp
 
 data HandWrittenGrevlex = HandWrittenGrevlex
 
+toSeq :: U.Unbox a => U.Vector a -> Seq a
+toSeq = Seq.fromList . U.toList
+
 instance SingI n => IsOrder n HandWrittenGrevlex where
-  cmpMonomial _ (unsized -> m) (unsized -> n) = grevlexHW m n 0 0 EQ
+  cmpMonomial _ (unsized -> m) (unsized -> n) = grevlexHW (toSeq m) (toSeq n) 0 0 EQ
   {-# INLINE cmpMonomial #-}
 
 instance SingI n => IsMonomialOrder n HandWrittenGrevlex
 
-
 i2 :: [OrderedPolynomial (Fraction Integer) Grevlex 5]
-i2 =  [35 * y^4 - 30*x*y^2 - 210*y^2*z + 3*x^2 + 30*x*z - 105*z^2 +140*y*t - 21*u
-      ,5*x*y^3 - 140*y^3*z - 3*x^2*y + 45*x*y*z - 420*y*z^2 + 210*y^2*t -25*x*t + 70*z*t + 126*y*u
-      ]
-    where [t,u,x,y,z] = vars
+i2 =
+  [ 35 * y ^ 4 - 30 * x * y ^ 2 - 210 * y ^ 2 * z + 3 * x ^ 2 + 30 * x * z - 105 * z ^ 2 + 140 * y * t - 21 * u
+  , 5 * x * y ^ 3 - 140 * y ^ 3 * z - 3 * x ^ 2 * y + 45 * x * y * z - 420 * y * z ^ 2 + 210 * y ^ 2 * t -25 * x * t + 70 * z * t + 126 * y * u
+  ]
+  where
+    [t, u, x, y, z] = vars
 
 mkTC :: (IsMonomialOrder 5 ord) => String -> ord -> Benchmark
 mkTC name ord =
   env (return $ toIdeal $ map (changeOrder ord) i2) $ \ideal ->
-  bgroup name [ bench "calcGroebnerBasis" $ nf calcGroebnerBasis ideal
-              , bench "F5" $ nf f5 ideal
-              ]
+    bgroup
+      name
+      [ bench "calcGroebnerBasis" $ nf calcGroebnerBasis ideal
+      , bench "F5" $ nf f5 ideal
+      ]
 
 main :: IO ()
 main =
-
-  defaultMainWith defaultConfig
+  defaultMain
     [ mkTC "default" Grevlex
     , mkTC "foldl" FoldlGrevlex
     , mkTC "foldl-custom" FoldlSeq'Grevlex
